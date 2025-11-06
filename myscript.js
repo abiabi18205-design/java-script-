b733a7665cd59e94903a5621396308eb3710e450
abi_shanth <abiabi18205@gmail.com>
2025-11-06 13:56:21 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,16 @@
+console.log("apisanth")
+console.log("christian college")
+console.log("watching movies")
+console.log(100+100)
+
+
+let name="abi";
+var age="20";
+const subject="english";
+
+console.log(name)
+console.log(age)
+console.log(subject)
+
+let favcolor=prompt("input your favourite color");
+document.body.style.backgroundColor=favcolor;
