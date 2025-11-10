e739f0adc0a5ed79578ceb05d38f3bb87c759b48
abi_shanth <abiabi18205@gmail.com>
2025-11-10 11:46:32 +0530

    hoisting

--- myscript.js
+++ myscript.js
@@ -1,3 +1,5 @@
+// javascript variables
+
 console.log("apisanth")
 console.log("christian college")
 console.log("watching movies")
@@ -12,5 +14,37 @@ console.log(name)
 console.log(age)
 console.log(subject)
 
-let favcolor=prompt("input your favourite color");
-document.body.style.backgroundColor=favcolor;
+// let favcolor=prompt("input your favourite color");
+// document.body.style.backgroundColor=favcolor;
+
+
+let color=""
+console.log(color)
+
+
+let firstname="abi";
+let lastname="shanth";
+
+console.log("my name is"+" "+firstname+" "+lastname)
+
+
+let ages="20";
+console.log(typeof ages);
+
+
+
+
+{
+    let x=25;
+    console.log(x)
+}
+
+
+    var x=25;
+    console.log(x)
+
+
+{
+    const x=25;
+    console.log(x)
+}
